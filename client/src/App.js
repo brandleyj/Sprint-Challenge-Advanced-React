f3ea722986bee3a810dfe0ab8a951d5b912dba74
Justin Brandley <brandleyj@gmail.com>
2019-10-04 10:41:10 -0500

    built app.js and axios request

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,31 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
 
-function App() {
-	return <div className="App"></div>;
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			users: []
+		};
+	}
+
+	componentDidMount() {
+		axios.get("http://localhost:5000/api/players").then(res => {
+			console.log(res.data);
+			this.setState({
+				users: [...this.state.users, res.data]
+			});
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<p>hello</p>
+			</div>
+		);
+	}
 }
 
 export default App;
